b92f66e5bde0817f78c912051d1ad61d5637ce79
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-03 21:35:26 +0100

    Add --base-dir and --pattern command line args

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Collections.Generic;
 using System.Linq;
 using McMaster.Extensions.CommandLineUtils;
 using System.IO;
@@ -14,24 +15,34 @@ namespace dotnet_versioninfo
         Description = "Display version information of .NET Core assemblies.")]
     class Program
     {
+        private const string DEFAULT_BASE_DIR = ".";
+        private const string DEFAULT_PATTERN = "**/*.dll";
+
         public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option(Description = "Base directory to which the glob pattern is applied [current directory]")]
+        [DirectoryExists]
+        public string BaseDir { get; }
+
+        [Option(Description = "Glob pattern [**/*.dll]")]
+        public string Pattern { get; }
+
+        [Option(Description = "Show relative paths in the results [false]")]
+        public bool Relative { get; }
 
         [Option(Description = "Display the version of this tool and then exit")]
         public bool Version { get; }
 
-        [Option(Description = "Show relative paths in the results")]
-        public bool Relative { get; }
-
         private void ProcessFile(string fileName)
         {
-            var fvi = FileVersionInfo.GetVersionInfo(fileName);
             Console.WriteLine($"{fileName}");
+            var fvi = FileVersionInfo.GetVersionInfo(fileName);
             Console.WriteLine($"\tFileVersion:\t{fvi.FileVersion}");
             Console.WriteLine($"\tProductVersion:\t{fvi.ProductVersion}");
         }
 
-        private Func<string, string> ToRelativePath(string relativeTo) =>
-            (string path) => Path.GetRelativePath(relativeTo, path);
+        private Func<string, string> ToRelativePath(string relativeBaseDir, string absoluteBaseDir) =>
+            (string path) => Path.Combine(relativeBaseDir, Path.GetRelativePath(absoluteBaseDir, path));
 
         private T Identity<T>(T t) => t;
 
@@ -42,6 +53,12 @@ namespace dotnet_versioninfo
             Console.WriteLine($"{aiva.InformationalVersion}");
         }
 
+        private static IEnumerable<FileInfo> GlobFilesWorkaround(DirectoryInfo di, string pattern) {
+            var glob = new Glob.Glob(pattern, GlobOptions.Compiled);
+            return di.EnumerateFiles("*", SearchOption.TopDirectoryOnly)
+                .Where(directory => glob.IsMatch(directory.FullName));
+        }
+
         private int OnExecute()
         {
             if (Version) {
@@ -49,17 +66,23 @@ namespace dotnet_versioninfo
                 return 0;
             }
 
-            var baseDir = ".";
-            var pattern = "**/*.dll";
-            var absoluteBaseDir = Path.GetFullPath(baseDir);
+            var actualBaseDir = BaseDir ?? DEFAULT_BASE_DIR;
+            var actualPattern = Pattern ?? DEFAULT_PATTERN;
+
+            var absoluteBaseDir = Path.GetFullPath(actualBaseDir);
             var pathTransformer = Relative
-                ? ToRelativePath(absoluteBaseDir)
+                ? ToRelativePath(actualBaseDir, absoluteBaseDir)
                 : Identity;
             var directoryInfo = new DirectoryInfo(absoluteBaseDir);
-            var fileNames = directoryInfo.GlobFiles(pattern)
-                .Select(fileInfo => fileInfo.FullName)
-                .Select(pathTransformer)
-                .ToList();
+            var fileNames = actualPattern.Contains("**")
+                ? directoryInfo.GlobFiles(actualPattern)
+                    .Select(fileInfo => fileInfo.FullName)
+                    .Select(pathTransformer)
+                    .ToList()
+                : GlobFilesWorkaround(directoryInfo, actualPattern)
+                    .Select(fileInfo => fileInfo.FullName)
+                    .Select(pathTransformer)
+                    .ToList();
             fileNames.ForEach(ProcessFile);
             return 0;
         }
