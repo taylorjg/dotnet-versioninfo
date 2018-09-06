2ca8fdb34a0bca0b460cbedd3d4524ada12ac317
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-06 10:24:52 +0100

    Add scripts to install, update and uninstall from local dir or NuGet

---
+++ scripts/install-from-local.sh
@@ -0,0 +1,5 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TOP="${DIR}/.."
+
+dotnet pack "${TOP}/src/dotnet-versioninfo.csproj" --output "${TOP}/dist"
+dotnet tool install --global dotnet-versioninfo --add-source "${TOP}/dist"
--- update.sh
+++
@@ -1,2 +0,0 @@
-dotnet pack src/dotnet-versioninfo.csproj --output .
-dotnet tool update --global dotnet-versioninfo --add-source .
